88b85c030fc539542afb09175d3bd716404e6fc0
Dlzxn <aleshamarichev09@gmail.com>
2025-12-07 21:49:45 +0300

    +middleware

--- src/service/market/checking_item_price.py
+++ src/service/market/checking_item_price.py
@@ -1,32 +1,46 @@
-def checking_item_price(user_id: int, item_id: int, currently_price: int | float):
+import json
+
+
+def checking_item_price(user_id: int, item_id: int, last_price: int | float):
     """
         Summary: Проверка стоимости скина
 
         Parameters:
             user_id: int - ID пользователя
             item_id: str - название скина
-            currently_price: int | float - текущая цена
+            last_price: int | float - последняя рекомендованная цена маркета
         
         Return:
             Any
     """
-
     with open('data/checking_item_price.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    
     if item_id in data:
-        user = data[item_id]['user']
-
-        if user == user_id:
-            return 'Надо поменять цену скина'
-
+        if user_id == data[item_id]['user_id']:
+            data[item_id] = {
+                "value": last_price - 0.01,
+                "user_id": user_id,
+            }
+            with open('data/checking_item_price.json', 'w', encoding='utf-8') as f:
+                json.dump(data, f, indent=4)
+            return 1
+        if last_price < data[item_id]["value"]:
+            data[item_id] = {
+                "value": last_price - 0.01,
+                "user_id": user_id,
+            }
+            with open('data/checking_item_price.json', 'w', encoding='utf-8') as f:
+                json.dump(data, f, indent=4)
+            return 1
         else:
-            price = data[item_id]['price']
+            return 0
 
-            if currently_price > price:
-                return f'Надо поменять цена на {price + 0.01}'
-
-            return 'Наша цена самая маленькая'
-
-    return 'Скина нет'
+    else:
+        data[item_id] = {
+            "value": last_price - 0.01,
+            "user_id": user_id,
+        }
+        with open('data/checking_item_price.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4)
+        return 1
--- src/service/scheduler/trader.py
+++ src/service/scheduler/trader.py
@@ -5,6 +5,7 @@ from apscheduler.jobstores.base import JobLookupError
 from src.service.logger_cfg.log import logger
 from src.service.market.api.cs_market import CSMarket
 from src.db.CRUD import user_database
+from src.service.market.checking_item_price import checking_item_price
 
 async def check_user_orders(user_id: int | str) -> None:
     time_start = time.time()
@@ -16,6 +17,9 @@ async def check_user_orders(user_id: int | str) -> None:
         list = await market.get_items_for_sale()
 
         for x in user["skins"]:
+            if not x["auto_reprice"]:
+                logger.info("Данный скин не торгуется")
+                continue
             for y in list["items"]:
                 if str(y["item_id"]) == str(x["id"]):
                     hash_name = y["market_hash_name"]
@@ -34,11 +38,18 @@ async def check_user_orders(user_id: int | str) -> None:
                                 logger.info(f"Нашли цену в {price}")
                                 flag = True
                                 break
+                        status = checking_item_price(user_id, y["item_id"], price)
+                        match(status):
+                            case 0:
+                                price += 2
+                            case 1:
+                                pass
+                        flag_1 = False
                         if price < x["min_price"]:
                             price = x["min_price"]
-                            flag = True
                         print(f"Айди:{y["item_id"]}")
-                        if flag and price != y["price"]:
+                        print(price, y["price"]*100)
+                        if price != y["price"]*100:
                             status = await market.update_price_item(item_id=y["item_id"], new_price_item=price)
                             if status["status"]:
                                 logger.info(f"Цена на предмет {hash_name} изменена на {price}")
