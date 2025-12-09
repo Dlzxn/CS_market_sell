7c5bcff7f9e8a7a9e3a0109e9bd9465e0261f29a
Dlzxn <aleshamarichev09@gmail.com>
2025-12-09 18:27:04 +0300

    fix

--- start_thread.py
+++ start_thread.py
@@ -19,10 +19,10 @@ def start():
     for user_id, _ in all_id:
         check_file = open(f"data/app_check_{user_id}.log", 'w', encoding='utf-8')
         del_file = open(f"data/app_del_{user_id}.log", 'w', encoding='utf-8')
-        proc_check = subprocess.Popen([ENV_PYTHON, "src/app_check.py", str(user_id)],
+        proc_check = subprocess.Popen([ENV_PYTHON, "app_check.py", str(user_id)],
                          stdout=check_file,
                          stderr=check_file,)
-        proc_del = subprocess.Popen([ENV_PYTHON, "src/app_del.py", str(user_id)],
+        proc_del = subprocess.Popen([ENV_PYTHON, "app_del.py", str(user_id)],
                          stdout=del_file,
                          stderr=del_file
                          )
