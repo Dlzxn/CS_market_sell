0ce3182c2b670511dbc58ef79abb2912a1a8df82
Dlzxn <aleshamarichev09@gmail.com>
2025-12-09 18:14:12 +0300

    multiproc

---
+++ src/app_check.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from  fastapi.responses import FileResponse
+import httpx, asyncio, sys
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.base import JobLookupError
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime, timedelta
+
+from src.model.DataModel import DataModel, UpdateTimeData
+from src.Routers.user_rout import user_rout
+from src.db.CRUD import user_database
+from src.service.scheduler.trader import check_user_orders, delete_skins, check_new_skins
+from src.service.logger_cfg.log import logger
+
+scheduler = AsyncIOScheduler()
+
+
+def startup_event(user_id):
+    """Запускает планировщик при старте сервера."""
+    print("Процесс с айди ",user_id)
+    user_id = int(user_id)
+    if not scheduler.running:
+        scheduler.start()
+    scheduler.add_job(
+            check_new_skins,
+            'interval',
+            seconds=1800,
+            args=[user_id],
+            id="check_new_skins: " + str(user_id),
+            max_instances=1,
+            next_run_time=datetime.now() + timedelta(seconds=1)
+        )
+    print("APScheduler запущен и готов к работе.")
+
+async def main():
+    """Асинхронная функция для запуска планировщика и ожидания."""
+    user_id = sys.argv[1]
+    startup_event(user_id)
+    try:
+        while True:
+            await asyncio.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+        print("Планировщик остановлен.")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ start_thread.py
@@ -0,0 +1,37 @@
+import subprocess, sys, time, os
+
+from src.db.CRUD import user_database
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+ENV_PYTHON = os.path.join(PROJECT_ROOT, ".venv", "bin", "python")
+
+def start():
+    if not os.path.exists(ENV_PYTHON):
+        print(f"❌ Ошибка: Интерпретатор не найден по пути: {ENV_PYTHON}", file=sys.stderr)
+        print("Убедитесь, что виртуальное окружение (.venv) создано и активировано.", file=sys.stderr)
+        sys.exit(1)
+
+
+    all_id = user_database.get_all_id()
+    proc_list = []
+    for user_id, _ in all_id:
+        check_file = open(f"data/app_check_{user_id}.log", 'w', encoding='utf-8')
+        del_file = open(f"data/app_del_{user_id}.log", 'w', encoding='utf-8')
+        proc_check = subprocess.Popen([ENV_PYTHON, "src/app_check.py", str(user_id)],
+                         stdout=check_file,
+                         stderr=check_file,)
+        proc_del = subprocess.Popen([ENV_PYTHON, "src/app_del.py", str(user_id)],
+                         stdout=del_file,
+                         stderr=del_file
+                         )
+        proc_list.append((proc_check, proc_del))
+    print(len(proc_list))
+    time.sleep(5)
+    for proc_check, proc_del in proc_list:
+        proc_check.terminate()
+        proc_del.terminate()
+    start()
+
+start()
